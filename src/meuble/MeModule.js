b6e425cc88a0ada7522caa9e580e415ded85cc28
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2025-09-05 09:20:15 +0200

    Creation de la classe MeModule et inclusion du fichier dans .html et test

---
+++ src/meuble/MeModule.js
@@ -0,0 +1,11 @@
+
+
+class MeModule
+{
+    constructor( largeur, hauteur, profondeur )
+    {
+        this.hauteur = hauteur ;
+        this.largeur = largeur ;
+        this.profondeur = profondeur ;
+    }
+}
