9d82d123320e426ec312d55c3f0d478b3c6476ff
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2025-09-05 11:45:01 +0200

    Test unitaire sur MeModule et les valeurs d'attribut ok

--- test/meuble/MeModule.test.js
+++ test/meuble/MeModule.test.js
@@ -3,5 +3,25 @@ import { MeModule } from "../../src/meuble/MeModule";
 
 describe( "MeModule test sur les valeurs d'attibut", function()
 {
+    let m1 = new MeModule( 100, 350, 50 ) ;
+
+
+    test( "Largeur doit être >= à 30 et =< à 120", function()
+    {
+        expect( m1.largeur ).toBeGreaterThanOrEqual( 30 ) ;
+        expect( m1.largeur ).toBeLessThanOrEqual( 120 ) ;
+    } );
+
+    test( "Hauteur doit être >= à 10 et =< à 300", function()
+    {
+        expect( m1.hauteur ).toBeGreaterThanOrEqual( 10 ) ;
+        expect( m1.hauteur ).toBeLessThanOrEqual( 300 ) ;
+    } ) ;
+
+    test( "Profondeur doit être >= à 30 et =< à 120", function()
+    {
+        expect( m1.profondeur ).toBeGreaterThanOrEqual( 30 ) ;
+        expect( m1.profondeur ).toBeLessThanOrEqual( 120 ) ;
+    } ) ;
 
 }) ;
