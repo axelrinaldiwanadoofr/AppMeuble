ac3e36840c3a5b3918f75584659ab431119fe058
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2025-09-11 09:18:58 +0200

    Ajout d'un bug sur largeur

--- test/meuble/MeModule.test.js
+++ test/meuble/MeModule.test.js
@@ -1,10 +1,11 @@
 
 import { MeModule } from "../../src/meuble/MeModule";
 
-describe( "MeModule test sur les valeurs d'attibut", function()
+describe( "MeModule test sur les valeurs d'attribut", function()
 {
     let m1 = new MeModule( 100, 350, 50 ) ;
 
+    m1.largeur = 500 ;
 
     test( "Largeur doit être >= à 30 et =< à 120", function()
     {
